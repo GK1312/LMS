14ebb1fdef5dd2afe6871dc98c731ca82abc5edf
GK-Promodome <163818119+GK-Promodome@users.noreply.github.com>
2024-05-14 22:28:53 +0530

    minor changes

--- src/app/components/auth/OtpForm.tsx
+++ src/app/components/auth/OtpForm.tsx
@@ -32,52 +32,56 @@ const OtpForm = React.memo(function OtpForm() {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col items-stretch justify-start gap-3"
-    >
-      <div className="flex items-center justify-center gap-3">
-        {Array.from({ length: 6 }, (_, index) => (
-          <Controller
-            key={index}
-            name={`otp.${index}`}
-            control={control}
-            rules={{ required: true, maxLength: 1 }}
-            render={({ field, fieldState: { error } }) => (
-              <input
-                {...field}
-                ref={(el) => {
-                  inputRefs.current[index] = el;
-                }}
-                type="text"
-                maxLength={1}
-                onKeyUp={(e) => moveToNextField(index, e.currentTarget.value)}
-                autoComplete="one-time-code"
-                inputMode="numeric"
-                className={`w-full max-w-14 h-10 px-4 py-2 text-label leading-tight font-normal text-center border border-solid border-hr outline outline-1 outline-transparent rounded-md focus:shadow-input-focus ${
-                  Object.keys(errors).length > 0 ? "shadow-input-danger" : ""
-                } transition-all duration-200 ease-in-out`}
-              />
-            )}
-          />
-        ))}
-      </div>
-      {Object.keys(errors).length > 0 && (
-        <div className={`input_error_box ${!isValid ? "h-6" : "h-0"} relative`}>
-          {!isValid && (
-            <p className="mt-1 text-label text-danger leading-tight font-medium flex gap-1 absolute top-0 visible">
-              Otp has to be 6 digit.
-            </p>
-          )}
-        </div>
-      )}
-      <button
-        type="submit"
-        className="w-full min-h-9 px-5 py-2 text-xs text-white leading-none tracking-wider font-semibold uppercase inline-flex items-center justify-center bottom-0 outline-0 bg-accent select-none rounded-md transition-all duration-100 ease-in-out hover:bg-accent-dark"
+    <>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col items-stretch justify-start gap-3"
       >
-        Verify OTP
-      </button>
-    </form>
+        <div className="flex items-center justify-center gap-3">
+          {Array.from({ length: 6 }, (_, index) => (
+            <Controller
+              key={index}
+              name={`otp.${index}`}
+              control={control}
+              rules={{ required: true, maxLength: 1 }}
+              render={({ field, fieldState: { error } }) => (
+                <input
+                  {...field}
+                  ref={(el) => {
+                    inputRefs.current[index] = el;
+                  }}
+                  type="text"
+                  maxLength={1}
+                  onKeyUp={(e) => moveToNextField(index, e.currentTarget.value)}
+                  autoComplete="one-time-code"
+                  inputMode="numeric"
+                  className={`w-full max-w-14 h-10 px-4 py-2 text-label leading-tight font-normal text-center border border-solid border-hr outline outline-1 outline-transparent rounded-md focus:shadow-input-focus ${
+                    Object.keys(errors).length > 0 ? "shadow-input-danger" : ""
+                  } transition-all duration-200 ease-in-out`}
+                />
+              )}
+            />
+          ))}
+        </div>
+        {Object.keys(errors).length > 0 && (
+          <div
+            className={`input_error_box ${!isValid ? "h-6" : "h-0"} relative`}
+          >
+            {!isValid && (
+              <p className="mt-1 text-label text-danger leading-tight font-medium flex gap-1 absolute top-0 visible">
+                Otp has to be 6 digit.
+              </p>
+            )}
+          </div>
+        )}
+        <button
+          type="submit"
+          className="w-full min-h-9 px-5 py-2 text-xs text-white leading-none tracking-wider font-semibold uppercase inline-flex items-center justify-center bottom-0 outline-0 bg-accent-theme select-none rounded-md transition-all duration-100 ease-in-out hover:bg-accent-theme-dark"
+        >
+          Verify OTP
+        </button>
+      </form>
+    </>
   );
 });
 
